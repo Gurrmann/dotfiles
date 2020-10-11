1d4193f0e0648e0fbf7804261d6c762d276687bc
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-10-11 22:16:50 +0200

    replaced scrot with flameshot

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install feh compton i3blocks scrot xclip vim neofetch mpv lxappearance ranger htop pavucontrol
+sudo apt install feh compton i3blocks flameshot xclip vim neofetch mpv lxappearance ranger htop pavucontrol
 sudo apt autoremove
