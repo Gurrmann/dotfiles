218c99a7ead86baf9c07394f3254bf189e570790
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-10-12 05:12:36 +0200

    Readded powerline shell

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install feh compton i3blocks flameshot xclip vim neofetch mpv lxappearance ranger htop pavucontrol
+sudo apt install feh compton i3blocks flameshot xclip vim neofetch mpv lxappearance ranger htop pavucontrol powerline python3-pip
+sudo pip3 install powerline-shell
 sudo apt autoremove
