f3a15e2c3de500f677c40d681bcc8b0060c6621e
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-10-14 03:47:07 +0200

    added i3-gaps

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install feh compton i3blocks flameshot xclip vim neofetch mpv lxappearance ranger htop pavucontrol powerline python3-pip
+sudo add-apt-repository ppa:kgilmer/speed-ricer
+sudo apt install feh compton i3-gaps i3blocks flameshot xclip vim neofetch mpv lxappearance ranger htop pavucontrol powerline python3-pip
 sudo pip3 install powerline-shell
 sudo apt autoremove
