b3a22d884706ee96df0bc047a54da29e62ab5539
Gustav <gustav.skallberg@gmail.com>
2020-10-22 15:32:38 +0200

    Fixed error in vim move script

--- scripts/vimMover.sh
+++ scripts/vimMover.sh
@@ -2,8 +2,8 @@
 
 cd ~/ 
 test -d oldConfigs/ || mkdir oldConfigs
-test -d .vim/ && mv .vim/ oldConfigs
+test -d .vim/ && cp -rf .vim/ oldConfigs && rm -rf .vim/
 
-cd ~/dotfiles 
+cd ~/dotfiles/
 cp -rf .vim/ ~/
 
